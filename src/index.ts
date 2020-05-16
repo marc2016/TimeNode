dcd491ac78c9c79ef46cd3046faa955d4c972b47
Marc Lammers <marc@lammers.dev>
2020-05-16 20:56:56 +0200

    Setup typeorm

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
+import { createConnection } from 'typeorm'
+
 var express = require('express')
 var app = express()
 
-app.get('/', function (_req: any, res: { send: (arg0: string) => void }) {
-  res.send('Hello ds!')
-})
+async function initServer() {
+  await createConnection()
+  app.get('/', function (_req: any, res: { send: (arg0: string) => void }) {
+    res.send('Hello ds!')
+  })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+  })
+}
+
+initServer()
