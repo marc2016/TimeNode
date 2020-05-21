d47eb663d4e97f10836caa161d815808d1431caa
Marc Lammers <marc@lammers.dev>
2020-05-21 22:25:24 +0200

    Swagger added.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+
+  const options = new DocumentBuilder()
+    .setTitle('TimeNode')
+    .setDescription('The TimeNode API description')
+    .setVersion('1.0')
+    .addTag('time')
+    .build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('api', app, document)
+
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()
