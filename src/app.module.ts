c4fb20f6305eafe168bd51ca49fae196ae183470
Marc Lammers <marc@lammers.dev>
2020-05-27 22:08:44 +0200

    Modules added to app.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { TimersModule } from './timers/timers.module'
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), TimersModule, UsersModule],
+  imports: [TypeOrmModule.forRoot(), TimersModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService]
 })
