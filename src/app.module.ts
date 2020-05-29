409e18bc745462ba28880920827105fa6b328063
Marc Lammers <marc@lammers.dev>
2020-05-29 22:00:58 +0200

    Nestjs Config package added.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { TimersModule } from './timers/timers.module'
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), TimersModule, UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(),
+    TimersModule,
+    UsersModule,
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
