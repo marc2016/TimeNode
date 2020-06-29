d70635ad74714d77bfd15160143e1d24d4798a94
Marc Lammers <marc@lammers.dev>
2020-06-29 22:43:53 +0200

    TimeEntry service methods added.
    find methods added.

--- src/time-entry/time-entry.service.ts
+++ src/time-entry/time-entry.service.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@nestjs/common'
+import { TimeEntry } from './time-entry.entity'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
 
 @Injectable()
 export class TimeEntryService {
+  constructor(
+    @InjectRepository(TimeEntry)
+    private timeEntryRepository: Repository<TimeEntry>
+  ) {}
+
   async findAllByTask(taskId: string) {
-    // const tasks = await this.tasksRepository.find({
-    //   relations: ['user'],
-    //   where: { user: { id: userId } }
-    // })
-    // return tasks
+    const timeEntries = await this.timeEntryRepository.find({
+      relations: ['task'],
+      where: { task: { id: taskId } }
+    })
+    return timeEntries
+  }
+
+  async findAllByDate(date: Date) {
+    const timeEntries = await this.timeEntryRepository.find({
+      relations: ['task'],
+      where: { date: date }
+    })
+    return timeEntries
   }
 }
